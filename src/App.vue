0992bdf2dd91e968144635ce8b6aacdd478b7d2e
Vinícius Castelani dos Santos <visacastelani@gmail.com>
2022-02-04 11:44:10 -0300

    feat: initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-main> </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  methods: {},
+};
+</script>
+
+<style></style>
